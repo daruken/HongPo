a5ca30f91d56aa4139d2e87d071cc241a38b3c88
JeonHongYeol <daruken@hongyeolui-macbook-pro.local>
2018-08-29 21:34:14 +0900

    Initial Commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by HongYeol Jeon on 2018. 8. 29..
+//  Copyright © 2018년 HongYeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
