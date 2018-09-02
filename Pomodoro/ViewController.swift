0768592af4b8d3150af7f7037a52692220f15452
JeonHongYeol <daruken@hongyeolui-macbook-pro.local>
2018-09-02 15:03:07 +0900

    HongTen created github "HongPo"

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -9,10 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timerLabel: UILabel!
+    var hpTimer: Timer!
+    var totalTime = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let strokeTextAttributes: [NSAttributedStringKey: Any] = [
+            .strokeColor : UIColor.black,
+            .foregroundColor : UIColor.black,
+            .strokeWidth : -2.0,
+            .font : UIFont.boldSystemFont(ofSize: 18)
+        ]
+
+        timerLabel.attributedText = NSMutableAttributedString(string: "Hello?", attributes: strokeTextAttributes)
+        startTimer()
+
+    }
+    
+    func startTimer() {
+        hpTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+
+    }
+    
+    @objc func updateTime() {
+        timerLabel.text = "\(timeFormatted(totalTime))"
+        
+        if totalTime != 0 {
+            totalTime -= 1
+        } else {
+            endTimer()
+        }
+    }
+    
+    func endTimer() {
+        hpTimer.invalidate()
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        //     let hours: Int = totalSeconds / 3600
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 
     override func didReceiveMemoryWarning() {
