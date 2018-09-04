155d751e0a5d7ba4fcb8069ad0a0bbb184cb6c39
JeonHongYeol <daruken@hongyeolui-macbook-pro.local>
2018-09-04 20:43:55 +0900

    Added button(play | pause | stop)

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -10,48 +10,77 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    
     var hpTimer: Timer!
-    var totalTime = 60
+    var totalTime = 5
+    var playBtnFlag = 0;
+    
+    let TIMERPLAYING = 1
+    let TIMERPAUSE = 0
+    let TIMERSTOP = 2
+    
+    
+    @IBAction func playBtnOnClick(_ sender: Any) {
+        if(playBtnFlag == TIMERPAUSE){
+            playButton.setImage(UIImage(named: "pause.png"), for: .normal)
+            playButton.tintColor = UIColor.black
+            startTimer()
+            
+            playBtnFlag = TIMERPLAYING
+        }else if(playBtnFlag == TIMERPLAYING){
+            playButton.setImage(UIImage(named: "play.png"), for: .normal)
+            playButton.tintColor = UIColor.black
+            stopTimer()
+            
+            playBtnFlag = TIMERPAUSE
+        }else{
+            playButton.setImage(UIImage(named: "play.png"), for: .normal)
+            playButton.tintColor = UIColor.black
+            totalTime = 5
+            setTimerLabel()
+            
+            playBtnFlag = TIMERPAUSE
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let strokeTextAttributes: [NSAttributedStringKey: Any] = [
-            .strokeColor : UIColor.black,
-            .foregroundColor : UIColor.black,
-            .strokeWidth : -2.0,
-            .font : UIFont.boldSystemFont(ofSize: 18)
-        ]
-
-        timerLabel.attributedText = NSMutableAttributedString(string: "Hello?", attributes: strokeTextAttributes)
-        startTimer()
-
+        playButton.setImage(UIImage(named: "play.png"), for: .normal)
+        playButton.tintColor = UIColor.black
+        
+        setTimerLabel()
     }
     
     func startTimer() {
         hpTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-
     }
     
     @objc func updateTime() {
-        timerLabel.text = "\(timeFormatted(totalTime))"
-        
         if totalTime != 0 {
             totalTime -= 1
         } else {
-            endTimer()
+            playButton.setImage(UIImage(named: "stop.png"), for: .normal)
+            playButton.tintColor = UIColor.black
+            playBtnFlag = TIMERSTOP
+
+            stopTimer()
         }
+        
+        setTimerLabel()
     }
-    
-    func endTimer() {
+
+    func stopTimer() {
         hpTimer.invalidate()
     }
     
     func timeFormatted(_ totalSeconds: Int) -> String {
         let seconds: Int = totalSeconds % 60
         let minutes: Int = (totalSeconds / 60) % 60
-        //     let hours: Int = totalSeconds / 3600
+        
         return String(format: "%02d:%02d", minutes, seconds)
     }
 
@@ -60,6 +89,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setTimerLabel() {
+        let strokeTextAttributes: [NSAttributedStringKey: Any] = [
+            .strokeColor : UIColor.black,
+            .foregroundColor : UIColor.black,
+            .strokeWidth : -2.0,
+            .font : UIFont.boldSystemFont(ofSize: 64)
+        ]
+        
+        timerLabel.textAlignment = .center
+        timerLabel.attributedText = NSMutableAttributedString(string: "\(timeFormatted(totalTime))", attributes: strokeTextAttributes)
+    }
 }
 
