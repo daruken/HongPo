2946327c1988397c74e7f33d589f07b0e1748103
JeonHongYeol <daruken@hongyeolui-macbook-pro.local>
2018-10-09 19:03:14 +0900

    Added navigation view controller.

---
+++ Pomodoro/SettingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingViewController.swift
+//  Pomodoro
+//
+//  Created by HongYeol Jeon on 2018. 10. 9..
+//  Copyright © 2018년 HongYeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
     
     var hpTimer: Timer!
     var totalTime = 5
@@ -76,13 +78,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        playButton.frame.origin = CGPoint(x: 140, y: 295)
-        playButton.setImage(UIImage(named: "play.png"), for: .normal)
-        playButton.tintColor = UIColor.black
         
-        stopButton.isHidden = true
-        stopButton.setImage(UIImage(named: "stop.png"), for: .normal)
-        stopButton.tintColor = UIColor.black
+        buttonImgLoad()
         
         setTimerLabel()
     }
@@ -108,6 +105,7 @@ class ViewController: UIViewController {
             playBtnFlag = TIMERPLAYING
 
             stopTimer()
+            callAlarm()
         }
         
         setTimerLabel()
@@ -130,7 +128,7 @@ class ViewController: UIViewController {
     }
 
     func setTimerLabel() {
-        let strokeTextAttributes: [NSAttributedStringKey: Any] = [
+        let strokeTextAttributes: [NSAttributedString.Key: Any] = [
             .strokeColor : UIColor.black,
             .foregroundColor : UIColor.black,
             .strokeWidth : -2.0,
@@ -139,6 +137,24 @@ class ViewController: UIViewController {
         
         timerLabel.textAlignment = .center
         timerLabel.attributedText = NSMutableAttributedString(string: "\(timeFormatted(totalTime))", attributes: strokeTextAttributes)
+    }
+    
+    func buttonImgLoad() {
+        playButton.frame.origin = CGPoint(x: 140, y: 295)
+        playButton.setImage(UIImage(named: "play.png"), for: .normal)
+        playButton.tintColor = UIColor.black
+        
+        stopButton.isHidden = true
+        stopButton.setImage(UIImage(named: "stop.png"), for: .normal)
+        stopButton.tintColor = UIColor.black
+        
+        settingButton.frame.origin = CGPoint(x: 325, y: 80)
+        settingButton.setImage(UIImage(named: "setting.png"), for: .normal)
+        settingButton.tintColor = UIColor.black
+    }
+    
+    func callAlarm() {
+        AudioServicesPlaySystemSound(4095)
     }
 }
 
